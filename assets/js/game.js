ebb22bd2c3410f423eeba43571549d0f5717708f
loch <nigelloch@gmail.com>
2021-11-15 14:04:44 -0600

    first commit

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+window.alert("This is an alert! JavaScript is running!");
+window.prompt("what is your robot's name?");
+
+//this creates a function names "fight"
+
+function fight () {
+    window.alert("The fight has begun!");
+    }
+
+fight ();
