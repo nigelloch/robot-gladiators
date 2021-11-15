39460f1bfa1dc6b5bf9cf8206a09d76e61910f53
loch <nigelloch@gmail.com>
2021-11-15 14:25:31 -0600

    added playername function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,12 @@
 window.alert("This is an alert! JavaScript is running!");
-window.prompt("what is your robot's name?");
+var playerName = window.prompt("what is your robot's name?");
+//Note the lack of quotationmarks around playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
 
 //this creates a function names "fight"
 
@@ -7,4 +14,4 @@ function fight () {
     window.alert("The fight has begun!");
     }
 
-fight ();
+// fight ();
